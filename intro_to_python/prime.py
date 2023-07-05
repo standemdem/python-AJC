605106badbd993056a7069a6da61fca74b4c39c0
standemdem <stan.demontmarin@gmail.com>
2023-07-05 20:19:04 +0200

    fixed is_prime function

--- intro_to_python/prime.py
+++ intro_to_python/prime.py
@@ -1,14 +1,18 @@
-def is_prime(n):
-  for i in range(2,int(n/2)):
-    if (n%i) == 0:
-      return False
-  return True
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
 
 min = int(input("entre un nombre superieur ou égal à 2: "))
 max = int(input("entre un nombre plus grand: "))
 prime=[]
 
-for i in range(min, max):
+for i in range(min, max+1):
   if is_prime(i):
     prime.append(i)
 print(prime)
