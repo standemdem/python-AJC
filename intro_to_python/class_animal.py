b98f4b1b5934844250909f680129e9be3b37c74b
standemdem <stan.demontmarin@gmail.com>
2023-07-06 11:55:11 +0200

    added new class

--- intro_to_python/class_animal.py
+++ intro_to_python/class_animal.py
@@ -6,13 +6,42 @@ class Animal:
     def manger(self):
         print(f"{self.name} likes to eat {self.food}")
 
-dogo = [12, "Pedro", "dog", 45, "domestic", "poop"]
+class MeatEater(Animal):
+
+    def manger(self):
+        self.food="meat"
+        print(f"{self.name} likes to eat {self.food}")
+
+class SavageMeatEater(MeatEater):
+
+    def my_method(self):
+        """ classe d'animaux sauvages qui vivent aussi dans la forêt"""
+        pass
+
+    def speak(self):
+        print("I leave in the forest")
+        
+
 def init_class(animal_description):
     keys_list=["age", "name", "breed", "weight", "category", "food"]
     return dict(zip(keys_list, animal_description)).values()
 
-new1=Animal(init_class(dogo))
+dogo = [12, "Pedro", "dog", 45, "domestic", "poop"]
+loupio = [14, "Loupio", "wolf", 60, "savage", "little children"]
 
-print(new1.__dict__.keys())
-print(new1.__dict__.values())
-new1.manger()
+
+dog = Animal(init_class(dogo))
+
+print(dog.__dict__.keys())
+print(dog.__dict__.values())
+dog.manger()
+
+wolf = MeatEater(init_class(loupio))
+print(wolf.__dict__.keys())
+print(wolf.__dict__.values())
+wolf.manger()
+
+
+savage = SavageMeatEater(init_class(loupio))
+print(savage.my_method.__doc__)
+savage.speak()
